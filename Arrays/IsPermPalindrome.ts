783409fa3213b4cfb8bda3feb97ec15df2188f2d
anjali_ahlawat <anjaliahlawat16@gmail.com>
2024-03-31 18:38:08 +0000

    code for isPermPalindrome

--- Arrays/IsPermPalindrome.ts
+++ Arrays/IsPermPalindrome.ts
@@ -1,15 +1,34 @@
 // check if the given string is the permutation of a palindrome
 
-export function isPermPalindrome(str: string): boolean {
-  if (str.length === 0) return false;
-  const obj = {};
+function isPermPalindrome(str: string): boolean {
+  const cleanedStr = str.toLowerCase().replace(/ /g, "");
+  if (cleanedStr.length === 0) return false;
+  const obj: { [key: string]: number } = {};
 
-  for (let i = 0; i < str.length; i++) {
-    if (obj[str[i]]) {
-      obj[str[i]] += 1;
+  for (let i = 0; i < cleanedStr.length; i++) {
+    if (obj[cleanedStr[i]]) {
+      obj[cleanedStr[i]] += 1;
     } else {
-      obj[str[i]] = 1;
+      obj[cleanedStr[i]] = 1;
+    }
+  }
+
+  let repeatedCharCount = 0;
+  let uniqueCharCount = 0;
+
+  for (const element of Object.values(obj)) {
+    if (element === 1 && uniqueCharCount > 0) {
+      return false;
+    } else if (element === 1) {
+      uniqueCharCount += 1;
+    } else if (element > 1 && repeatedCharCount === 0) {
+      repeatedCharCount = element;
+    } else if (element > 1 && element !== repeatedCharCount) {
+      return false;
     }
   }
   return true;
 }
+
+// console.log(isPermPalindrome("sbc"));
+console.log(isPermPalindrome("Tact coa"));
