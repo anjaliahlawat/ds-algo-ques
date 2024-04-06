8d08c35f45a71dcd1b56b9f1778b6e56732a310e
anjaliahlawat <anjali.ahlawat@wolterskluwer.com>
2024-04-06 11:42:19 +0200

    added string ques and zero-matrix ques

--- Arrays/StringCompression.ts
+++ Arrays/StringCompression.ts
@@ -2,26 +2,19 @@
 // i/p -> aabbbccccaa
 // o/p -> a2b3c4a2
 
-function generateCompressedStr(obj: { [key: string]: number }): string {
-  let compressedStr = "";
-  for (const [key, value] of Object.entries(obj)) {
-    compressedStr += key + value;
-  }
-  return compressedStr;
-}
-
 function compressStr(str: string): string {
-  let count = 0;
+  let updatedStr = "";
+  let count = 1;
 
-  let obj: { [key: string]: number } = {};
   for (let i = 0; i < str.length; i++) {
-    if (obj[str[i]]) {
-      obj[str[i]] += 1;
+    if (i > 0 && str[i] === str[i - 1]) {
+      count++;
     } else {
-      obj[str[i]] = 1;
+      updatedStr += str[i - 1] + count;
+      count = 1;
     }
   }
-  return generateCompressedStr(obj);
+  return updatedStr;
 }
 
 console.log(compressStr("aabbbccccaa"));
---
+++ Arrays/ZeroMatrix.ts
@@ -0,0 +1,38 @@
+//
+
+function ZeroMatixValues(matrix: number[][], size: number): number[][] {
+  const indices = getZeroIndicesFromMatrix(matrix);
+  let updatedMatrix = [...matrix];
+
+  for (let m = 0; m < indices.length; m++) {
+    updatedMatrix = multipleByZero(updatedMatrix, size, indices[m]);
+  }
+  return updatedMatrix;
+}
+
+function getZeroIndicesFromMatrix(matrix: number[][]): number[][] {
+  const indices = [];
+  for (let i = 0; i < matrix.length; i++) {
+    for (let j = 0; j < matrix[0].length; j++) {
+      if (matrix[i][j] === 0) {
+        indices.push([i, j]);
+      }
+    }
+  }
+  return indices;
+}
+
+function multipleByZero(
+  matrix: number[][],
+  size: number,
+  [rowIndex, colIndex]: number[]
+): number[][] {
+  let zeroedMatrix = [...matrix];
+  for (let j = 0; j < size; j++) {
+    zeroedMatrix[j][colIndex] = 0;
+  }
+  for (let j = 0; j < size; j++) {
+    zeroedMatrix[rowIndex][j] = 0;
+  }
+  return zeroedMatrix;
+}
